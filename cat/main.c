545cdc9f907ddd56d7e9e9113f7b53b50cf348a3
Abel <59095867+BlessedOneKobo@users.noreply.github.com>
2024-03-27 10:56:53 +0100

    feat(cat): add c bare-bones c implementation

--- cat/main.c
+++ cat/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-	printf("hello, world\n");
-	return 0;
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    // TODO: print usage
+    return EXIT_FAILURE;
+  }
+
+  char *filename = argv[1];
+  FILE *fp = fopen(filename, "r");
+  int ch = 0;
+
+  while ((ch = fgetc(fp)) != EOF)
+    putchar(ch);
+  fclose(fp);
 }
